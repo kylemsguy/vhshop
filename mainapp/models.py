9a62a77b8076cc8ca517b78f3506bbf6371a76e0
Reinaesaya <xseanhylx@gmail.com>
2014-02-22 14:07:45 -0500

    Second Commit

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Company(models.Model):
+    CompName = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.CompName
+
+class Glasses(models.Model):
+    company = models.ForeignKey(Company)
+    GlassesName = models.CharField(max_length=200)
+    likes = models.IntegerField(default=0)
+    numtries = models.IntegerField(default=0)
+    pictures = models.ImageField(upload_to='glasses')
+    def numtries_to_likes(self):
+        return self.numtries/self.likes
+    def __str__(self):
+        return self.GlassesName
+    
--- vhshop/settings.py
+++ vhshop/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mainapp',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -58,7 +59,7 @@ WSGI_APPLICATION = 'vhshop.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'maindb.sqlite3'),
     }
 }
 
@@ -80,3 +81,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = 'C:\Users\Sean\Desktop\McHacks\vhshop\templates\images'
